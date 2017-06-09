d235e992811bbe05b441b2d04ec4db7eb55a5c96
Nikolay Ushmodin <ushmodin@gmail.com>
2017-06-09 11:34:46 +0700

    Передача персональных данных

--- src/main/java/com/artmark/avs5router/model/TransitConfirmRequest.java
+++ src/main/java/com/artmark/avs5router/model/TransitConfirmRequest.java
@@ -21,6 +21,7 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="RouteKey" type="{}RouteKey"/>
+ *         &lt;element name="ticketId" type="{}IDType" maxOccurs="unbounded"/>
  *         &lt;element name="Ticket" maxOccurs="unbounded">
  *           &lt;complexType>
  *             &lt;complexContent>
@@ -45,6 +46,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
     "routeKey",
+    "ticketId",
     "ticket",
     "agent"
 })
@@ -53,6 +55,8 @@ public class TransitConfirmRequest {
 
     @XmlElement(name = "RouteKey", required = true)
     protected RouteKey routeKey;
+    @XmlElement(required = true)
+    protected List<String> ticketId;
     @XmlElement(name = "Ticket", required = true)
     protected List<TransitConfirmRequest.Ticket> ticket;
     @XmlElement(name = "Agent")
@@ -80,6 +84,35 @@ public class TransitConfirmRequest {
      */
     public void setRouteKey(RouteKey value) {
         this.routeKey = value;
+    }
+
+    /**
+     * Gets the value of the ticketId property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the ticketId property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getTicketId().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<String> getTicketId() {
+        if (ticketId == null) {
+            ticketId = new ArrayList<String>();
+        }
+        return this.ticketId;
     }
 
     /**
